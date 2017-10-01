807c65ceac6f683589c32ba675c9d38dad7f79d4
Salah Kaci <salah91@gmail.com>
2017-10-01 18:49:56 +0200

    [List] Add repository cell class & layout

--- augment-github/RepositoriesViewController.swift
+++ augment-github/RepositoriesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class RepositoriesViewController: UITableViewController {
 
+    
 
 }
 
---
+++ augment-github/RepositoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  RepositoryCell.swift
+//  augment-github
+//
+//  Created by Salah on 2017-10-01.
+//  Copyright © 2017 DevSK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+    
+    static let identifier = "RepositoryCellIdentifier"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
